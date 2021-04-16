a10ba2ec9a67149e12991b44a5ca9117d2342ad2
moontai0724 <me@moontai0724.tw>
2021-04-16 15:28:04 +0800

    feat: config uses .env file

--- src/main.module.ts
+++ src/main.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './configuration';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema: Joi.object({}),
+    }),
+  ],
 })
 export class MainModule {}
