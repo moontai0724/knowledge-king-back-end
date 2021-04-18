c2c3aa363b9e8f7452deae2ee60d219839a2ab70
moontai0724 <me@moontai0724.tw>
2021-04-18 15:47:49 +0800

    feat: database basic config & config validation remove package 'Joi', using custom validate function instead. add package 'class-validation' & 'class-trsnaformer'.

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+export default () => ({
+  environment: process.env.NODE_ENV,
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT) || 3306,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
+    timezone: process.env.DATABASE_TIMEZONE,
+  },
+});
--- src/main.module.ts
+++ src/main.module.ts
@@ -1,14 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import configuration from './configuration';
-import * as Joi from 'joi';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from 'src/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { validate, Environment } from 'src/env.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
-      validationSchema: Joi.object({}),
+      validate,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('database.host'),
+        port: +configService.get<number>('database.port'),
+        username: configService.get('database.user'),
+        password: configService.get('database.password'),
+        database: configService.get('database.database'),
+        timezone: configService.get('database.timezone'),
+        synchronize:
+          configService.get('environment') === Environment.Production,
+      }),
+      inject: [ConfigService],
     }),
   ],
 })
